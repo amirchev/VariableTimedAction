6a8b6cf5f48f5504f23fde9e99a6d81ae2f49d9f
amirchev <alexmirchev@hotmail.com>
2018-06-25 18:05:35 -0700

    undo remove init

--- VariableTimedAction.cpp
+++ VariableTimedAction.cpp
@@ -6,10 +6,17 @@
 
 #include <VariableTimedAction.h>
 
-int VariableTimedAction::maxActions = 15;
-VariableTimedAction* VariableTimedAction::actions[VariableTimedAction::maxActions](NULL);
+int VariableTimedAction::maxActions;
+VariableTimedAction ** VariableTimedAction::actions;
 
 void VariableTimedAction::start(unsigned long startInterval, bool startNow = true) {
+    static bool init = false;
+    if (!init) {
+        maxActions = 15;
+        actions = new VariableTimedAction*[maxActions](NULL); //start at 15 actions, increase as needed
+        init = true;
+    }
+    
     if (index == -1) {
         int emptyIndex = -1;
         for (int i = 0; i < maxActions; i++) {
